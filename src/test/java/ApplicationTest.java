539bb44077652110f13e54a0bed32fe55dde4667
nSafari <nsafarinia@it-street.cologne>
2021-05-15 14:14:31 +0200

    Adding more tests

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,6 +1,10 @@
 import commands.PlaceOrderCommand;
+import commands.ShipOrderCommand;
+import events.OrderConfirmedEvent;
 import events.OrderPlacedEvent;
+import events.OrderShippedEvent;
 import java.util.UUID;
+import org.axonframework.messaging.annotation.MessageHandlerInvocationException;
 import org.axonframework.test.aggregate.AggregateTestFixture;
 import org.axonframework.test.aggregate.FixtureConfiguration;
 import org.junit.Before;
@@ -16,12 +20,35 @@ public class ApplicationTest {
     }
 
     @Test
-    public void firstTest(){
+    public void placeAnOrderShouldPublishOrderPlacedEvent(){
 
         String orderId = UUID.randomUUID().toString();
         String product = "Deluxe Chair";
         fixture.givenNoPriorActivity()
                 .when(new PlaceOrderCommand(orderId, product))
                 .expectEvents(new OrderPlacedEvent(orderId, product));
+
+    }
+
+    @Test
+    public void shipOrderCommandShouldThrowAnExceptionWhenTheOrderIsNotConfirmedYet(){
+
+        String orderId = UUID.randomUUID().toString();
+        String product = "Deluxe Chair";
+        fixture.given(new OrderPlacedEvent(orderId, product))
+                .when(new ShipOrderCommand(orderId))
+                .expectException(MessageHandlerInvocationException.class);
+
+    }
+
+    @Test
+    public void placeOrderCommandShouldThrowAnExceptionWhenTheProductIsNotDefinedYet(){
+
+        String orderId = UUID.randomUUID().toString();
+        String product = "Deluxe Chair";
+        fixture.given(new OrderPlacedEvent(orderId, product), new OrderConfirmedEvent(orderId))
+                .when(new ShipOrderCommand(orderId))
+                .expectEvents(new OrderShippedEvent(orderId));
+
     }
 }
